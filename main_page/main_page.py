8faedd4804f919392bc36046cb37b5d2efc8caf5
dolomaniuk <dalamas200487@gmail.com>
2018-11-13 22:12:21 +0300

    вызов функции создания предзаявки

--- main_page/main_page.py
+++ main_page/main_page.py
@@ -8,7 +8,8 @@ text = """
 """
 
 from change_connect_settings.change_connect_settings import choice_thing
-from change_request_status.change_status_request import change_status
+# from change_request_status.change_status_request import change_status
+from prerequest.create_prerequest import request
 
 path = "connect_to_base.ini"
 
@@ -27,7 +28,10 @@ def case(*args):
 
 print(text)
 print('1. Изменить настройки соединеня\n2. Изменить статус заявки\n3. Создать предзаявку\n4. Поиск отсутствующих файлов')
-choice = int(input('Выберите один из вариантов\n'))
+try:
+    choice = int(input('Выберите один из вариантов\n'))
+except ValueError:
+    choice = 0
 
 while switch(choice):
     if case(1):
@@ -38,9 +42,12 @@ while switch(choice):
         print("Изменить статус заявки")
         change_status()
         break
-
+    if case(3):
+        print("Создание предзаявки")
+        request()
+        break
     print("Указали некорректный вариант")
     break
-
+    request()
     print("Указали некорректный вариант")
     break
