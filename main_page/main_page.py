d5eaf0691bc8ca38ac68fea592acb205647a6537
Dmitry <dalamas200487@gmail.com>
2018-11-21 15:40:06 +0300

    добавил ф-ю вывода карточек клиента

--- main_page/main_page.py
+++ main_page/main_page.py
@@ -10,6 +10,7 @@ text = """
 from change_connect_settings.change_connect_settings import choice_thing
 from change_request_status.change_status_request import change_status
 from prerequest.create_prerequest import request
+from check_status_card_in_sv.check_status_card_in_sv import send_request_to_SV
 
 path = "connect_to_base.ini"
 
@@ -27,7 +28,7 @@ def case(*args):
 
 
 print(text)
-print('1. Изменить настройки соединеня\n2. Изменить статус заявки\n3. Создать предзаявку\n')
+print('1. Изменить настройки соединеня\n2. Изменить статус заявки\n3. Создать предзаявку\n4. Карточки клиента')
 # print('1. Изменить настройки соединеня\n2. Изменить статус заявки\n3. Создать предзаявку\n4. Поиск отсутствующих файлов')
 try:
     choice = int(input('Выберите один из вариантов\n'))
@@ -46,6 +47,10 @@ while switch(choice):
     if case(3):
         print("Создание предзаявки")
         request()
+        break
+    if case(4):
+        print("Карточки клиента")
+        send_request_to_SV()
         break
     print("Указали некорректный вариант")
     break
