3d3b87b6c3488ce1aa4d4699ab3c0bf1fc3936fb
Dmitry <dalamas200487@gmail.com>
2019-01-11 01:21:33 +0300

    добавил проверку необходимых файлов

---
+++ main_page/check_nes_files.py
@@ -0,0 +1,26 @@
+import os
+
+
+def check_files():
+    db_dir = 'sql_requests\\'
+    xml_dir = 'xml_request\\'
+    db_requests = [
+        'cards.sql',
+        'Fp_card_balance.sql',
+        'ppl_code.sql'
+        ]
+    xml_requests = [
+        'SV_card_status.xml',
+        'SV_check_balance.xml'
+        ]
+    err_list = []
+    if not os.path.isfile('connections.ini'):
+        err_list.append('connections.ini')
+    for file in db_requests:
+        if not os.path.isfile(db_dir + file):
+            err_list.append(file)
+    for file in xml_requests:
+        if not os.path.isfile(xml_dir + file):
+            err_list.append(file)
+    if err_list:
+        print("Для полноценной работы необходимы следующие файлы:", *err_list, sep='\n', end='\n')
--- main_page/fastQA.py
+++ main_page/fastQA.py
@@ -13,6 +13,7 @@ from change_connect_settings.change_connect_settings import choice_thing
 from prerequest.create_prerequest import request
 from check_status_card_in_sv.check_status_card_in_sv import check_balance_SV_FP, check_status_SV
 from coy.coy_operation import send_coy_request
+from main_page.check_nes_files import check_files
 
 path = "connections.ini"
 
@@ -20,6 +21,7 @@ path = "connections.ini"
 # объявление 2х функций для switch-case
 class _switch(object):
     value = None
+
     def __new__(class_, value):
         class_.value = value
         return True
@@ -29,12 +31,12 @@ def _case(*args):
     return any((arg == _switch.value for arg in args))
 
 
-print(text)
-
 while 1:
+    check_files()  # проверка необходимых файлов
     def_con = ' '.join([*ini.get_config_parameters(path, "DEFAULT")])
     print(
         """
+        
         1. Изменить настройки соединеня (""" + def_con + """)
         2. Изменить статус заявки
         3. Проверить установку обновлений в db
@@ -50,38 +52,59 @@ while 1:
     while _switch(choice):
         if _case(1):
             print("Изменить настройки соединеня")
-            choice_thing()
-            print('\n\n')
-            break
+            try:
+                choice_thing()
+            except:
+                print("Не удалось выполнить команду")
+            finally:
+                break
         if _case(2):
             print("Изменить статус заявки")
-            db.change_status()
-            print('\n\n')
-            break
+            try:
+                db.change_status()
+            except:
+                print("Не удалось выполнить команду")
+            finally:
+                break
         if _case(3):
             print("Проверить установку обновлений в db")
-            db.find_update()
-            print('\n\n')
-            break
+            try:
+                db.find_update()
+            except:
+                print("Не удалось выполнить команду")
+            finally:
+                break
         if _case(4):
             print("Создание предзаявки")
-            request()
-            print('\n\n')
-            break
+            try:
+                request()
+            except:
+                print("Не удалось выполнить команду")
+            finally:
+                break
         if _case(5):
             print("Статус карточек клиента в SV")
-            check_status_SV()
-            print('\n\n')
-            break
+            try:
+                check_status_SV()
+            except:
+                print("Не удалось выполнить команду")
+            finally:
+                break
         if _case(6):
             print("Баланс карточки по номеру договора")
-            check_balance_SV_FP()
-            print('\n\n')
-            break
+            try:
+                check_balance_SV_FP()
+            except:
+                print("Не удалось выполнить команду")
+            finally:
+                break
         if _case(7):
             print("Инфа о клиенте в СОУ")
-            send_coy_request()
-            print('\n\n')
-            break
+            try:
+                send_coy_request()
+            except:
+                print("Не удалось выполнить команду")
+            finally:
+                break
         print("Указали некорректный вариант")
         break
